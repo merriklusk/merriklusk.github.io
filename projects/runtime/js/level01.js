6637038dbdf0ba50d7a81b90542166e2cc09146b
merriklusk <mjlusk420@gmail.com>
2022-03-16 14:14:37 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,9 +16,19 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "sawblade", "x": 400, "y": groundY },
-                { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "sawblade", "x": 400, "y": groundY - 50 },
+                { "type": "sawblade", "x": 600, "y": groundY - 50 },
+                { "type": "sawblade", "x": 900, "y": groundY - 50 },
+            
+                { "type": "enemy", "x": 500, "y": groundY - 50 },
+                { "type": "enemy", "x": 700, "y": groundY - 50 },
+                { "type": "enemy", "x": 900, "y": groundY - 50 },  
+                { "type": "enemy", "x": 1100, "y": groundY - 50 },
+               
+                { "type": "reward", "x": 800, "y": groundY - 50 },
+                { "type": "reward", "x": 950, "y": groundY - 50 },
+                { "type": "reward", "x": 1099, "y": groundY - 50 },  
+                { "type": "reward", "x": 1200, "y": groundY - 50 },
             ]
         };
         window.levelData = levelData;
@@ -40,12 +50,7 @@ var level01 = function (window) {
         obstacleImage.x = -25; //tweaks the image 25 pixels to the left
         obstacleImage.y = -25; //tweaks the image 25 pixels up 
             sawBladeHitZone.rotationalVelocity = 5;
-        }
-
-        createSawBlade(400, groundY - 50);
-        createSawBlade(600, groundY - 50);
-        createSawBlade(800, groundY - 50);        
-        
+        }       
         function createEnemy(x, y){
              var enemy = game.createGameItem('enemy',25); // creating the game item and storing it in the variable enemy
         var redSquare = draw.rect(50,50,'red'); //creates rectangle and stores as red square 
@@ -54,28 +59,62 @@ var level01 = function (window) {
         enemy.addChild(redSquare); //add the redsquare to the enemy game item
         
         enemy.x = x;
-        enemy.y = groundY = y;
+        enemy.y = y;
 
         game.addGameItem(enemy); //adds enemy to the game
 
         enemy.velocityX = -1; //this causes the enemey to move one pixel to the left on the x position 
-
+        enemy.rotationalVelocity = 100;
         enemy.onPlayerCollision = function() {
-        console.log('The enemy has hit Halle');
-        game.changeIntegrity(-10);
-};
-        enemy.onProjectileCollision = function() {
-        console.log('The projectile has hit Halle');
-        game.changeIntegrity(-10);
-        game.increaseScore(100);
-        enemy.fadeOut();
-};
-
+            console.log('The enemy has hit Halle');
+            game.changeIntegrity(-10);
         }
-       
-       createEnemy(400, groundY - 50);
-     createEnemy(400, groundY - 50);
-       createEnemy(400, groundY - 50);
+        enemy.onProjectileCollision = function() {
+            console.log('The projectile has hit Halle');
+            game.changeIntegrity(-10);
+            game.increaseScore(0);
+            enemy.fadeOut();
+        };
+        function createReward(x, y){
+        var reward = game.createGameItem('reward',25); // creating the game item and storing it in the variable enemy
+        var blueSquare = draw.rect(50,50,'blue'); //creates rectangle and stores as red square 
+        blueSquare.x = -25;
+        blueSquare.y = -25;
+        reward.addChild(blueSquare); //add the redsquare to the enemy game item
+        
+        reward.x = x;
+        reward.y = y;
+
+        game.addGameItem(reward); //adds enemy to the game
+
+        reward.velocityX = -1; //this causes the enemey to move one pixel to the left on the x position 
+        
+        reward.rotationalVelocity = 100;
+        
+        reward.onPlayerCollision = function() {
+            console.log('The reward has hit Halle');
+            game.changeIntegrity(-10);
+        }
+     
+};
+        
+        }
+    
+    for(var i = 0; i < levelData.gameItems.length; i++){
+         var gameItem = levelData.gameItems[i];
+
+        if(gameItem.type === "sawblade"){
+            createSawBlade(gameItem.x, gameItem.y);
+        }
+        if(gameItem.type === "enemy"){
+            createEnemy(gameItem.x, gameItem.y);
+        }
+        if(gameItem.type === "reward"){
+            createReward(gameItem.x, gameItem.y);
+        }
+    };
+
+
 
         // DO NOT EDIT CODE BELOW HERE
     }
