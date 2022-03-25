c9bf2b0d669f2ab903543683367197e375dd3f43
merriklusk <mjlusk420@gmail.com>
2022-03-25 14:03:42 +0000

    add runtime project

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,9 +16,9 @@ var level01 = function (window) {
             "number": 1, 
             "speed": -3,
             "gameItems": [
-                { "type": "sawblade", "x": 400, "y": groundY - 50 },
-                { "type": "sawblade", "x": 600, "y": groundY - 50 },
-                { "type": "sawblade", "x": 900, "y": groundY - 50 },
+                { "type": "sawblade", "x": 400, "y": groundY - 30 },
+                { "type": "sawblade", "x": 600, "y": groundY - 30 },
+                { "type": "sawblade", "x": 900, "y": groundY - 30 },
             
                 { "type": "enemy", "x": 500, "y": groundY  -160 },
                 { "type": "enemy", "x": 700, "y": groundY - 50 },
